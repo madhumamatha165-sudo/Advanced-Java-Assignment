74758220af5dcabfd8b6dab7c06e9ddf9a57332e
Mamatha J <madhumamatha165@gmail.com>
2026-03-09 22:09:49 +0530

    Create Stringdemo.java

---
+++ module4/Stringdemo.java
@@ -0,0 +1,16 @@
+public class StringDemo {
+    public static void main(String[] args) {
+
+        String s = "Hello";
+        s = s + " World";
+        System.out.println("String: " + s);
+
+        StringBuilder sb = new StringBuilder("Hello");
+        sb.append(" World");
+        System.out.println("StringBuilder: " + sb);
+
+        StringBuffer sbf = new StringBuffer("Hello");
+        sbf.append(" World");
+        System.out.println("StringBuffer: " + sbf);
+    }
+}
