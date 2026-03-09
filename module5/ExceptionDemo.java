22ba51f0744516cb92dfdf5b424267422185a1a9
Mamatha J <madhumamatha165@gmail.com>
2026-03-09 22:15:18 +0530

    Create ExceptionDemo.java

---
+++ module5/ExceptionDemo.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class ExceptionDemo {
+    public static void main(String[] args) {
+
+        try {
+            int a = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Unchecked Exception: " + e);
+        }
+
+        try {
+            FileReader f = new FileReader("test.txt");
+        } catch (IOException e) {
+            System.out.println("Checked Exception: " + e);
+        }
+    }
+}
