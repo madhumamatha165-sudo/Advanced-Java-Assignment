cca306139fd35b78048ed2e62a3a216669ce9ed8
Mamatha J <madhumamatha165@gmail.com>
2026-03-09 22:12:46 +0530

    Create garbageDemo.java

---
+++ module4/garbageDemo.java
@@ -0,0 +1,16 @@
+public class GarbageDemo {
+
+    protected void finalize() {
+        System.out.println("Garbage collected");
+    }
+
+    public static void main(String[] args) {
+        GarbageDemo obj1 = new GarbageDemo();
+        GarbageDemo obj2 = new GarbageDemo();
+
+        obj1 = null;
+        obj2 = null;
+
+        System.gc();
+    }
+}
